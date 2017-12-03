dd86d51ea34ef72561655b1cfd3e0fa349f930d0
hirose <hirose@posl.ait.kyushu-u.ac.jp>
2017-12-03 19:18:07 +0900

    add fetch data scripts

---
+++ data_generate.py
@@ -0,0 +1,138 @@
+#! /usr/bin/python3
+
+import json
+import glob
+
+
+def get_pick(path):
+    ret = {}
+    dec = None
+    with open(path) as f:
+        dec = json.load(f)
+    teams = {'1': [], '2': []}
+    for event in dec["pick"]:
+        action = event['type']
+        if action == "HeroSelect":
+            team = event['payload']['Team']
+            hero = event['payload']['Hero']
+            teams[team].append(hero)
+    ret["teams"] = teams
+
+    if dec["winner"] == "left/blue":
+        ret["winner"] = 1
+    else:
+        ret["winner"] = -1
+
+    return ret
+
+
+def get_ban_pick(path):
+    ret = {}
+    dec = None
+    with open(path) as f:
+        dec = json.load(f)
+    seq = []
+    action_set = set()
+    for event in dec["pick"]:
+        action = event['type']
+        team = event['payload']['Team']
+        hero = event['payload']['Hero']
+        seq.append({"action": action,
+                    "team": team,
+                    "hero": hero})
+        action_set.add(action)
+    if "HeroBan" not in action_set:
+        return None
+    ret["seq"] = seq
+
+    if dec["winner"] == "left/blue":
+        ret["winner"] = 1
+    else:
+        ret["winner"] = -1
+
+    return ret
+
+
+def ban_pick_list():
+    ranked_list = []
+    for path in glob.glob("data/pick/*.json"):
+        dic = get_ban_pick(path)
+        if dic is not None:
+            ranked_list.append(dic)
+
+    print("Sample count %d" % len(ranked_list))
+
+    for d in ranked_list:
+        data_list = []
+        for pick in d["seq"]:
+            hero = pick["hero"]
+            data_list.append(hero)
+        data_list.append(str(d["winner"]))
+        print(" ".join(data_list))
+
+
+def pick_dict():
+    pick_list = []
+    for path in glob.glob("data/pick/*.json"):
+        dic = get_pick(path)
+        if dic is not None:
+            pick_list.append(dic)
+
+    print("Sample count %d" % len(pick_list))
+
+    dic = {}
+    tmp_set = set()
+    for d in pick_list:
+        team1 = tuple(sorted(d["teams"]['1']))
+        team2 = tuple(sorted(d["teams"]['2']))
+
+        if team1 in tmp_set:
+            dic[team1].append(1 if d["winner"] == 1 else 0)
+        else:
+            dic[team1] = [1] if d["winner"] == 1 else [0]
+            tmp_set.add(team1)
+
+        if team2 in tmp_set:
+            dic[team2].append(1 if d["winner"] == -1 else 0)
+        else:
+            dic[team2] = [1] if d["winner"] == -1 else [0] 
+            tmp_set.add(team2)
+        
+    for team, rate in dic.items():
+        if len(rate) != 0:
+            print(str(team) + " %f" % (sum(rate) / len(rate)))
+        else:
+            print("No record")
+    print(len(dic))
+    return dic
+
+
+def get_win_rate(dic, pick_set):
+    mean = []
+    team_cmp = {}
+    for team, rate in dic.items():
+        if pick_set <= set(team):
+            mean.extend(rate)
+            win_rate  = sum(rate) / len(rate)
+            match_count = len(rate)
+            team_cmp[team] = (win_rate, match_count)
+    if len(mean) != 0:
+        print(pick_set, (sum(mean) / len(mean)), len(mean))
+    else:
+        print("No record")
+
+    print("Recommend")
+    for team, rate in sorted(team_cmp.items(), key=lambda x: x[1][0], reverse=True)[:10]:
+        print(team, rate)
+
+
+def main():
+    dic = pick_dict()
+    while True:
+        names = input("Hero names: ")
+        get_win_rate(dic, set(names.split()))
+    #ban_pick_list()
+
+
+if __name__ == "__main__":
+    main()
---
+++ telemetory.py
@@ -0,0 +1,115 @@
+#! /usr/bin/python3
+
+import requests
+import json
+import time
+
+APIKEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0M2Y4ZDliMC1iOGI5LTAxMzUtNjkwNC0wYTU4NjQ2MGFhYzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTEyMTI3OTYxLCJwdWIiOiJzZW1jIiwidGl0bGUiOiJ2YWluZ2xvcnkiLCJhcHAiOiJ2Z19ubiIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.IbSw6eT8OgF7Kn3Vk9QAttgoCkshH2PxGWqnAtbA5Is"
+
+header = {
+        "Authorization": APIKEY,
+        "Accept": "application/vnd.api+json"
+        }
+
+
+def get_matches(offset):
+    url = "https://api.dc01.gamelockerapp.com/shards/na/matches"
+    query = {
+            "filter[gameMode]": "ranked",
+            "page[offset]": offset,
+            "page[limit]": "5"
+            }
+
+    r = requests.get(url, headers=header, params=query)
+    IDs = []
+    for data in r.json()["data"]:
+        ID = data["id"]
+        asset_id = data["relationships"]["assets"]["data"][0]["id"]
+        IDs.append({"id": ID, "asset": asset_id})
+
+
+    with open("data/raw/raw_%d.json" % offset, "w") as f:
+        f.write(json.dumps(r.json(), indent=4))
+     
+    tel2ros = create_tel_to_roster_dict(r.json())
+
+    num = offset
+    tel_infos = get_telemetory_info(r.json())
+
+    for ID_obj in IDs:
+        ID = ID_obj["id"]
+        asset_id = ID_obj["asset"]
+
+        dic = {}
+        tel_url = tel_infos[asset_id]
+        seq = get_pick_seq(tel_url, asset_id)
+        dic["pick"] = seq
+
+        ros_id = tel2ros[asset_id]
+        win, side = is_win(r.json(), ros_id)
+        if win == "true":
+            dic["winner"] = side
+            dic["loser"] = "another"
+        else:
+            dic["winner"] = "another"
+            dic["loser"] = side
+
+        with open("data/pick/%s.json" % ID, "w") as f:
+            f.write(json.dumps(dic, indent=4))
+        num += 1
+        time.sleep(5)
+
+
+def is_win(match_obj, ros_id):
+    for data in match_obj["included"]:
+        if data["type"] == "roster":
+            if data["id"] == ros_id:
+                won = data["attributes"]["won"]
+                side = data["attributes"]["stats"]["side"]
+                return won, side
+
+def create_tel_to_roster_dict(match_obj):
+    dic = {}
+    for match in match_obj["data"]:
+        if match["type"] == "match":
+            asset_id = match["relationships"]["assets"]["data"][0]["id"]
+            roster_id = match["relationships"]["rosters"]["data"][0]["id"]
+            dic[asset_id] = roster_id
+    return dic
+
+def get_telemetory_info(match_obj):
+    urls = {}
+    for data in match_obj["included"]:
+        if data["type"] == "asset":
+            asset_id = data["id"]
+            print(data["attributes"]["URL"])
+            url = data["attributes"]["URL"]
+            urls[asset_id] = url
+    return urls
+
+
+def get_pick_seq(tel_url, ID):
+    ret = []
+    tel_res = requests.get(tel_url, headers=header)
+    
+    with open("data/tel/tel_%s.json" % ID, "w") as f:
+            f.write(json.dumps(tel_res.json(), indent=4))
+     
+    for payload in tel_res.json():
+        if payload["type"] == "HeroBan":
+            print(payload)
+            ret.append(payload)
+
+        if payload["type"] == "HeroSelect":
+            print(payload)
+            ret.append(payload)
+
+    return ret
+
+
+if __name__ == "__main__":
+    for i in range(0, 20000, 5):
+        get_matches(i)
+        time.sleep(5)
+
+#print(json.dumps(r.json(), indent=4))
