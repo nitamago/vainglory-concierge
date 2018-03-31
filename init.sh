76d3246d02497d431b6a494afcf0e694b278ff50
nitamago <nitamago@dti-vps-srv122.(none)>
2018-04-01 00:09:02 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ pip install -r requirements.txt
 pip install requests
 python manage.py migrate
 
-python clean_matches.py 0
-python clean_pick.py
-python create_heros.py
-python create_matches.py
+export PYTHONPATH=.
+python util/clean_matches.py 0
+python util/clean_pick.py
+python util/create_heros.py
 
+bash update_DB.sh
